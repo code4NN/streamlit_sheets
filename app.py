3ece3b844369951b3c733ebdfd03318f8e693b79
shivendra_work <shivendra.kumar@piramal.com>
2022-12-19 14:52:33 +0530

    api version

--- app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.markdown("## Hariboll")
-a = st.radio('choose',options=[1,2,3,4])
-st.markdown(f"you chose {a}")
---
+++ display.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+import g_api as gapi
+
+st.header('api access testing')
+
+sheetID = '1B5mc_TOPx04uGIYXDilb3oqd_CwZBb41eV6f-mCmQXA'
+sample_range = 'random!B1:C2'
+sample_range
+range =st.text_input('enter range')
+#--------------------
+
+values = gapi.getData(sheetID,sample_range)
+values
